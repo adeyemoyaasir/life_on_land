709fab830afe61d449cccbcd500a10f3166e74ed
WarutereIan <nmwanik111@gmail.com>
2023-03-07 20:43:11 +0300

    updated both currents to inital requirements

--- contracts/cast_votes.sol
+++ contracts/cast_votes.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity  >=0.7.0 <0.9.0;
 
 contract CastVotes {
@@ -12,6 +12,7 @@ contract CastVotes {
     struct  proposed_recipient {
         bytes32 name;
         uint voteCount;
+        address payable recipient_address;
     }
     
     address public chairperson;
@@ -21,20 +22,25 @@ contract CastVotes {
     //array of proposals
     proposed_recipient[] public proposals;
 
-
-    //creating ballot with the recipients to receive the funds
-    constructor(bytes32[] memory proposalNames) {
-        chairperson = msg.sender;
-        
-        for(uint i=0; i<proposalNames.length; i++){
-            proposals.push(proposed_recipient({
-                name: proposalNames[i],
-                voteCount: 0
-            }));
-        }
+     modifier limitAccessToOwner(){
+        require(chairperson == msg.sender,'function can only be called by chairperson');
+        _;
     }
 
-    //giving voters right to vote. require that voters be the only ones in original verified list
+    event proposalsAdded(
+        bool success,
+        proposed_recipient[] proposals
+
+    );
+
+
+    //creating ballot with the recipients to receive the funds
+    constructor() {
+        chairperson = msg.sender;       
+    }
+
+    //giving voters right to vote. chairperson currently has the power to select voters.
+    // This can be changed to something that uses a multisig wallet
 
     function giveVotingRight(address voter)  external {
         require(
@@ -48,6 +54,21 @@ contract CastVotes {
         require(voters[voter].weight == 0);
         voters[voter].weight = 1;
 
+    }
+
+    function addProposals(proposed_recipient[] memory proposals_Structs) public limitAccessToOwner() {
+
+        for(uint i=0; i<proposals_Structs.length; i++){
+            proposals.push(      
+                proposals_Structs[i]          
+            );
+        }
+
+        emit proposalsAdded(true, proposals);
+    }
+
+    function getArrayofProposals() public view returns( proposed_recipient[] memory){
+        return proposals;
     }
 
     function vote(uint proposal) external {
@@ -70,6 +91,12 @@ contract CastVotes {
 
     function winnerName() external view returns (bytes32 winnerName_){
         winnerName_ = proposals[winningProposal()].name;
+
+    }
+
+    function winnerAddress() external view returns (address winnerAddress_){
+        winnerAddress_=  proposals[winningProposal()].recipient_address;
+        
     }
 
 }
---
+++ contracts/donations.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 <0.9.0;
+
+contract DonateFunds {
+    uint256 totalDonationsAmount;
+    address payable owner;
+    uint256 highestDonation;
+    address payable  highestDonor;
+    address payable[] charities;
+
+    constructor(){
+        owner = payable (msg.sender);
+        totalDonationsAmount = 0;
+        highestDonation = 0;
+
+    }
+
+    modifier restrictedAccessToOwner (){
+        require(msg.sender == owner, 'Only owner can call this method');
+        _;
+    }
+
+    modifier validateRecipient (address payable recipientAddress){
+        require(msg.sender != recipientAddress,'sender address cannot be recipient address');
+        _;
+    }
+
+    modifier validateAmountSent (){
+        require( msg.value > 0, 'amount to send has to be greater than 0');
+        _;
+    }
+
+
+    modifier validateAmountDonated (){
+        require( msg.value > 0, 'amount to donate has to be greater than 0');
+        _;
+    }
+
+
+    event DonationReceived(
+        address indexed _donor,
+        uint256 _value
+    );
+
+    event DonationMade (
+        address _recipient_account,
+        uint256 _value
+    );
+
+    function donateToGeneralWallet() public validateAmountDonated() payable {
+        uint256 donationAmount = msg.value;
+        owner.transfer(donationAmount); //can contract hold funds..
+
+        emit DonationReceived(msg.sender, donationAmount);
+
+        totalDonationsAmount += donationAmount;
+        if(donationAmount > highestDonation){
+            highestDonation = donationAmount;
+            highestDonor = payable(msg.sender);
+        }
+    }
+
+    function donateToSpecificCause() public payable {
+
+    }
+
+    function addCharityAddress() public {
+
+    }
+
+    function removeCharityAddress() public {
+        
+    }
+
+    function getTotalDonations() view public returns(uint256){
+        return totalDonationsAmount;
+    }
+
+    function payOutDonations(address payable recipientAddress, uint256 donationAmount) public validateAmountSent() validateRecipient(recipientAddress) payable {
+        recipientAddress.transfer(donationAmount);
+        emit DonationMade(recipientAddress, donationAmount);
+    }
+
+}
+
+/* 
+    : pass recipient address from cast_votes.sol
+*/
